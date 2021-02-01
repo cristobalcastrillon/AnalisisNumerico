a4e9dad9644a9e466a45307b7843d51348bdf5b8
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-01-31 19:09:35 -0500

    Fixed bugs.

--- SeriesDeTaylor/SeriesTaylor.py
+++ SeriesDeTaylor/SeriesTaylor.py
@@ -8,12 +8,21 @@ def serieTaylor(n, a, f, val):
     "Calcula el valor de la serie de Taylor para la función 'f',"
     " teniendo en cuenta la 'n'-ésima derivada ('n'-ésimo término de la serie de Taylor)"
     " alrededor del valor 'a', evaluada en 'val'"
+    print("f(x):", f, "\ta:", a, "\tn:", n, "\tEvaluada en:", val)
 
     for i in range(0, n+1):
         f_prima = f.diff(x, i)
-        taylor = (f_prima.evalf(subs={x: a})*((val-a)**n))/ np.math.factorial(i)
+        # print("La ", i, "-ésima derivada de la función ", f, " es igual a ", f_prima)
+        taylor = (f_prima.evalf(subs={x: a})*((val-a)**i))/ np.math.factorial(i)
+        # print("La aproximación en el", i, "término de la serie es igual a ", taylor)
 
     return taylor
 
-print(serieTaylor(n = 2, a = 0.05, f = x**(1/2), val = 0.0088))
-print(serieTaylor(n = 12, a = 0.05, f = x**(1/2), val = 0.0088))
+puntoA = serieTaylor(n = 2, a = 0.05, f = x**(1/2), val = 0.0088)
+print(puntoA)
+
+puntoB = serieTaylor(n = 12, a = 0.05, f = x**(1/2), val = 0.0088)
+print(puntoB)
+
+# puntoC = serieTaylor(n = ??, a = ??, f = exp(x), val = 2.5)
+# print(puntoC)
