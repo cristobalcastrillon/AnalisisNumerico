85567fcebec3ab9b96be9426f8871dfdd5567acc
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-02-02 21:21:42 -0500

    Minor changes.

--- EcuacionCuadratica.py
+++ EcuacionCuadratica.py
@@ -1,14 +1,15 @@
-import cmath
 import math
+import cmath
 
 def evaluarCuadratica(a, b, c):
+    "Función que calcula los posibles resultados [x1, x2] (arreglo)"
+    "de un polinomio de la forma a*x^2 + b*x + c."
     exprInterna = ((b**2)-4*a*c)
     if(exprInterna < 0):
-        return [(-b+cmath.sqrt(exprInterna))/(2*a), (-b-cmath.sqrt(exprInterna))/(2*a)]
+        return [(-b + cmath.sqrt(exprInterna))/(2*a), (-b - cmath.sqrt(exprInterna))/(2*a)]
     else:
-        return [(-b+math.sqrt(exprInterna))/(2*a), (-b-math.sqrt(exprInterna))/(2*a)]
+        return [(-b + math.sqrt(exprInterna))/(2*a), (-b - math.sqrt(exprInterna))/(2*a)]
 
-print("Polinomio de la forma a*x^2 + b*x + c")
 print("Ingrese el valor de 'a': ")
 a = float(input())
 print("Ingrese el valor de 'b': ")
