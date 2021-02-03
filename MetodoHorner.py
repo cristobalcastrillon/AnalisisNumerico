2800233b85951f4c1f5ced8d81500ae5713fc1a7
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-02-03 14:14:27 -0500

    Added a function which creates a matrix useful for Horner's method implementation.

--- MetodoHorner.py
+++ MetodoHorner.py
@@ -9,6 +9,20 @@ def crearVectorPol(n):
         vec.append(i)
     return vec
 
+def crearMatrizPol(n, vec):
+    matriz = []
+    #ARREGLAR: Se está pasando una referencia a la lista 'vec', por lo que se está cambiando sus valores
+    for i in range(0, n):
+        matriz.append(vec)
+        # Imprimir la matriz si es necesario:
+        # print(vec)
+        for j in range(0, n+1):
+            if((vec[j] - 1) > 0):
+                vec[j] -= 1
+            else:
+                vec[j] = 0
+    return matriz
+
 def crearVectorCoef(n):
     vec = []
     for i in range(0, n+1):
@@ -29,10 +43,10 @@ n = int(input("Ingrese el grado del polinomio: "))
 grados = crearVectorPol(n)
 print(grados)
 
+matrizHorner = crearMatrizPol(n, grados)
+
 coeficientes = crearVectorCoef(n) 
 print(coeficientes)
 
 polinomio = formatoPolinomio(grados, coeficientes)
 print("p(x) = ", polinomio)
-
-formatoHorner(grados, coeficientes, polinomio)
