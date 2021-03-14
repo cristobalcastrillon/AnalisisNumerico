860efc48086e3cdda20d1fca1940c0ef2550ad73
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-03-14 16:37:24 -0500

    Se añadió la condición inicial que se evalúa para aplicar (o no) el método.

--- EntregaMitadParcial/interseccionCurvas/interseccionCurvas.py
+++ EntregaMitadParcial/interseccionCurvas/interseccionCurvas.py
@@ -2,16 +2,20 @@ import numpy as np
 import math 
 
 def interseccionCurvas(a, b, tol, f):
-    tramo = abs(b-a)
-    while(tramo > tol):        
-        c = b - (f(b) * (a - b))/(f(a) - f(b))
-        if(np.sign(f(c)) == np.sign(f(a))):
-            tramo = abs(c-a)
-            a = c
-        else:
-            tramo = abs(b-c)
-            b = c
-        print("APROX.=[{:0.10f}] | ERROR=[{}]".format(c,tramo))
+    #Se requiere que f(a) < f(b) en el intervalo [a,b].
+    if(f(a)-f(b) < 0 or f(b)-f(a) < 0):
+        tramo = abs(b-a)
+        while(tramo > tol):        
+            c = b - (f(b) * (a - b))/(f(a) - f(b))
+            if(np.sign(f(c)) == np.sign(f(a))):
+                tramo = abs(c-a)
+                a = c
+            else:
+                tramo = abs(b-c)
+                b = c
+            print("APROX.=[{:0.10f}] | ERROR=[{}]".format(c,tramo))
+    else:
+        print("No se cumplen las condiciones necesarias para aplicar el método.")
 
 #f: expresión hallada para la intersección entre las dos curvas.
 f = lambda x: -3*x**3 + 60*x + 57 - 300*x**-1 
