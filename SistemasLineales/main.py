cab3f4c88a51dfb155aad0fbc060d48ef37ef996
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-03-19 15:16:17 -0500

    Terminamos la solución al punto 2 (que nos correspondía) del taller de Sistemas de Ecuaciones Lineales.

--- SistemasLineales/main.py
+++ SistemasLineales/main.py
@@ -1,13 +1,12 @@
 import numpy as np
+from scipy.linalg import solve
 
 n = 3                      
 a = []                      
 b = []   
 
-def seidel(a, x, b): 
-          
+def seidel(a, x, b):           
     n = len(a)                    
-    
     for j in range(0, n):         
         d = b[j]                   
         for i in range(0, n):      
@@ -46,7 +45,30 @@ def radioEspectral(a):
   eigenValues = np.linalg.eigvals(t)
   #Calcular y retornar el valor máximo de 'eigenValues'.
   return max(eigenValues)
-    
+
+def jacobi(A, b, x, n):
+  y=[]
+  D = np.diag(A)
+  R = A-np.diagflat(D)
+  for i in range(n):
+      y=x
+      x = (b-np.dot(R,x))/D
+      print("({})->{}".format(i+1, x))
+      if(i > 0):
+        e1 = (x-y)
+        print("Vector error: ",abs(e1))
+  return x
+
+def sor(a,b,w,x):
+  for i in range(len(a)):
+    sigma=0
+    for j in range(len(a)):
+        if j != i:
+          sigma+=a[i][j]*x[j]
+    if(a[i][i]!=0):                 
+          x[i] += w*(((b[i]-sigma)/a[i][i])-x[i])
+  return x
+
 x = [0, 0, 0]                         
 a = [[4, 3, 0],[3, 4, -1],[0, -1, 4]] 
 #a1: matriz idéntica a 'a' para radio espectral.
@@ -57,16 +79,38 @@ a2 = [[4, 3, -2],[3, 4, -1],[0, -1, 4]]
 b = [0.254,-1.425,2.978] 
 
 print("PUNTO 2:\n")
-print("PUNTO A  ")
+print("--------------- PUNTO A -------------  ")
 print(radioEspectral(a1), '\n\n\n')
 
-print("PUNTO B  ")  
+print("--------------- PUNTO B -------------  ")  
 for i in range(0, 25):        
     x = seidel(a, x, b) 
     print(i,x) 
 print('\n\n\n')
 
-print("PUNTO C  ")
+print("--------------- PUNTO C ------------- ")
 for i in range(0, 25):        
     x = seidel(a2, x, b) 
     print(i,x) 
+print('\n\n\n')
+
+x=[0,0,0]
+print("--------------- PUNTO D -------------  ")
+w=0
+for j in range(0,15):
+  w+=0.125
+  print("W = ",w)
+  for i in range(0, 25):        
+      x = sor(a,b,w,x) 
+      print(i,x)
+  print(" ")
+
+print('\n\n')
+
+print("-------------- PUNTO E -------------  ")
+A = np.array([[4,3,0],[3,4,-1],[0,-1,4]])
+b = [0.254,-1.425,2.978]
+x = [1,2,3]
+n = 30
+x = jacobi(A, b, x, n)
+print("RESULTADO -> {}".format(solve(A,b)))
