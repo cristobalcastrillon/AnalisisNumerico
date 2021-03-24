002e9184036d5e018c52d944e8545ee9297de509
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-03-23 21:33:06 -0500

    Añadí el método de Diferencias Divididas de Newton. Falta hacer que imprima el polinomio y calcular el error.

--- Interpolacion/DiferenciasDivididas.py
+++ Interpolacion/DiferenciasDivididas.py
@@ -1,21 +1,55 @@
 import numpy as np
+#Sympy para imprimir el polinomio de Newton.
+#import sympy as sp
 
-def diferenciasDivididas(x, y):
-    c = []
-    for i in range(0, len(x)-1):
-        c.append((y[i+1]-y[i])/(x[i+1]-x[i]))
-        #La siguiente linea es de prueba
-        #print(b[i])
+def DiferenciasDivididas(vecX, vecY):
+    """
+    Función que calcula un polinomio de Newton que interpola las coordenadas [vecX, vecY].
+    """
+    n = len(vecX)+1
+    b = []
+    #Creando la matriz necesaria para efectuar las operaciones del método...
+    for i in range(len(vecX)):
+        c = [0 for z in range(n)]
+        b.append(c)
+        n -= 1
 
-def vector_fx(f, x):
-    y = []
-    for i in range(0, len(x)):
-        y.append(f(x))
-    #La siguiente linea es de prueba
-    print(y)
+    #Poblando la matriz con los valores correspondientes...
+    for j in range(len(vecX)):
+        for i in range(len(vecX)):
+            if(j == 0):
+                b[i][j] = vecX[i]
+            elif(j == 1):
+                b[i][j] = vecY[i]
 
-f = lambda x: np.log(x)
+    #Calculando la matriz (tabla) de diferencias divididas...
+    iVar = 1
+    jVar = 2
+    n = len(vecX)-1
+    for j in range(2, len(vecX)):
+        for i in range(n):
+            b[i][j] = (b[i + 1][j - 1] - b[i][j - 1]) / (b[i + iVar][j - jVar] - b[i][j - jVar])
+        iVar += 1
+        jVar += 1
+        n -= 1
+    print(b)
+
+    #Retornar / Imprimir el polinomio de Newton que interpola los puntos...
+
+def calcularVector(f, vecX):
+    vecY = []
+    for i in range(len(vecX)):
+        vecY.append(f(vecX[i]))
+    return vecY
+
+f1 = lambda x: np.log(x)
 x1 = [1, 2]
 
-f_x = vector_fx(f, x1)
-diferenciasDivididas(x1, f_x)
+f_x = calcularVector(f1, x1)
+DiferenciasDivididas(x1, f_x)
+
+# f2 = lambda x: x
+# x2 = [1,2,3,4]
+
+# f_x2 = calcularVector(f2, x2)
+# DiferenciasDivididas(x2, f_x2)
